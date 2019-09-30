08787b60c060bf4a7a7e7b5a4730628b81cd3bea
Minor Gordon <gordom6@rpi.edu>
2019-09-30 16:54:12 -0400

    failing test case

--- java/lib/src/test/java/edu/rpi/tw/twdb/lib/TestData.java
+++ java/lib/src/test/java/edu/rpi/tw/twdb/lib/TestData.java
@@ -6,11 +6,18 @@ import org.apache.jena.riot.RDFParserBuilder;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 
 public final class TestData {
     public TestData() throws IOException {
-        try (final InputStream inputStream = getClass().getClassLoader().getResourceAsStream("./nanopublication_without_provenance.trig")) {
-            RDFParserBuilder.create().source(inputStream).parse(nanopublicationWithoutProvenanceDataset);
+        parseDatasetFromResource(nanopublicationWithoutProvenanceDataset, "nanopublication_without_provenance.trig");
+    }
+
+    private void parseDatasetFromResource(final Dataset dataset, final String fileName) throws IOException {
+//        final String resourceName = "/" + getClass().getPackage().getName().replace(".", "/") + "/" + fileName;
+        final URL url = getClass().getResource("./" + fileName);
+        try (final InputStream inputStream = url.openStream()) {
+            RDFParserBuilder.create().base(url.toString()).source(inputStream).parse(dataset);
         }
     }
 
