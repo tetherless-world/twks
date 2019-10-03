3abc5c803325016e90bcde6e011d70b6d5ba4880
Minor Gordon <gordom6@rpi.edu>
2019-10-03 12:45:35 -0400

    first stab at a new query API

--- java/api/src/main/java/edu/rpi/tw/twdb/api/Twdb.java
+++ java/api/src/main/java/edu/rpi/tw/twdb/api/Twdb.java
@@ -2,6 +2,8 @@ package edu.rpi.tw.twdb.api;
 
 import edu.rpi.tw.nanopub.Nanopublication;
 import org.apache.jena.query.Dataset;
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.ReadWrite;
 import org.dmfs.rfc3986.Uri;
 
@@ -96,4 +98,24 @@ public interface Twdb {
      * @param transaction     existing transaction to use
      */
     void putNanopublication(Nanopublication nanopublication, TwdbTransaction transaction);
+
+    /**
+     * Query assertion parts of stored nanopublications.
+     * <p>
+     * See TwdbTest for examples on how to use this.
+     *
+     * @param query       query to execute. This will be augmented by the implementation as needed.
+     * @param transaction transaction this query will execute under
+     * @return QueryExecution that is ready to execute. Must be executed within the given transaction.
+     */
+    QueryExecution queryAssertions(Query query, TwdbTransaction transaction);
+
+    /**
+     * Query all parts of stored nanopublications (head, assertion, provenance, publication info).
+     *
+     * @param query       query to execute. This will be augmented by the implementation as needed.
+     * @param transaction transaction this query will execute under
+     * @return QueryExecution that is ready to execute. Must be executed within the given transaction.
+     */
+    QueryExecution queryNanopublications(Query query, TwdbTransaction transaction);
 }
