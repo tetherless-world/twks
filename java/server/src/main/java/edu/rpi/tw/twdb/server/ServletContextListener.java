14d1b55d26594848f59a2cc67f2773f3ca7b51c4
Minor Gordon <gordom6@rpi.edu>
2019-10-03 17:32:20 -0400

    first cut of SPARQL servlet

--- java/server/src/main/java/edu/rpi/tw/twdb/server/ServletContextListener.java
+++ java/server/src/main/java/edu/rpi/tw/twdb/server/ServletContextListener.java
@@ -3,6 +3,7 @@ package edu.rpi.tw.twdb.server;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.servlet.GuiceServletContextListener;
+import com.google.inject.servlet.ServletModule;
 
 public final class ServletContextListener extends GuiceServletContextListener {
     private Injector injector;
@@ -10,8 +11,15 @@ public final class ServletContextListener extends GuiceServletContextListener {
     @Override
     protected Injector getInjector() {
         if (injector == null) {
-            injector = Guice.createInjector();
+            injector = Guice.createInjector(new TwdbServletModule());
         }
         return injector;
+    }
+
+    private final static class TwdbServletModule extends ServletModule {
+        @Override
+        protected void configureServlets() {
+            super.configureServlets();
+        }
     }
 }
---
+++ java/server/src/main/java/edu/rpi/tw/twdb/server/servlet/sparql/SparqlServletModule.java
@@ -0,0 +1,6 @@
+package edu.rpi.tw.twdb.server.servlet.sparql;
+
+import com.google.inject.servlet.ServletModule;
+
+public final class SparqlServletModule extends ServletModule {
+}
