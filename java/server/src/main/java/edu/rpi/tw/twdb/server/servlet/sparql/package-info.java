1fa7041fc85fb064c48b329926fdf9c7278125ed
Minor Gordon <gordom6@rpi.edu>
2019-10-04 11:42:57 -0400

    non null by default

---
+++ java/server/src/main/java/edu/rpi/tw/twdb/server/servlet/sparql/package-info.java
@@ -0,0 +1,5 @@
+@DefaultAnnotation(NonNull.class)
+package edu.rpi.tw.twdb.server.servlet.sparql;
+
+import edu.umd.cs.findbugs.annotations.DefaultAnnotation;
+import edu.umd.cs.findbugs.annotations.NonNull;
