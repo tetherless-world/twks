3d2c3c7953fbc4de9408d45d244738b1395e48be
Minor Gordon <gordom6@rpi.edu>
2019-10-07 12:03:28 -0400

    implementing PUT

--- java/server/src/main/java/edu/rpi/tw/twdb/server/servlet/nanopublication/NanopublicationHttpServlet.java
+++ java/server/src/main/java/edu/rpi/tw/twdb/server/servlet/nanopublication/NanopublicationHttpServlet.java
@@ -1,14 +1,15 @@
 package edu.rpi.tw.twdb.server.servlet.nanopublication;
 
+import com.google.common.io.CharStreams;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import edu.rpi.tw.nanopub.Nanopublication;
-import edu.rpi.tw.nanopub.Uris;
+import edu.rpi.tw.nanopub.*;
 import edu.rpi.tw.twdb.api.Twdb;
 import edu.rpi.tw.twdb.server.servlet.TwdbHttpServlet;
 import edu.umd.cs.findbugs.annotations.Nullable;
 import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.riot.RDFLanguages;
 import org.dmfs.rfc3986.Uri;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,6 +19,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.Reader;
+import java.io.StringReader;
 import java.util.Optional;
 
 @Singleton
@@ -31,17 +34,16 @@ class NanopublicationHttpServlet extends TwdbHttpServlet {
 
     @Override
     protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
-        @Nullable final String pathInfo = req.getPathInfo();
-        if (pathInfo == null || pathInfo.length() < 2) {
+        final Optional<Uri> nanopublicationUri = getNanopublicationUri(req);
+        if (!nanopublicationUri.isPresent()) {
+            logger.info("no nanopublication URI specified in request");
             resp.sendError(HttpServletResponse.SC_NOT_FOUND);
             return;
         }
 
-        final Uri nanopublicationUri = Uris.parse(pathInfo.substring(1));
-
-        final Optional<Nanopublication> nanopublication = getDb().getNanopublication(nanopublicationUri);
+        final Optional<Nanopublication> nanopublication = getDb().getNanopublication(nanopublicationUri.get());
         if (!nanopublication.isPresent()) {
-            logger.info("nanopublication not found: {}", Uris.toString(nanopublicationUri));
+            logger.info("nanopublication not found: {}", Uris.toString(nanopublicationUri.get()));
             resp.sendError(HttpServletResponse.SC_NOT_FOUND);
             return;
         }
@@ -51,5 +53,74 @@ class NanopublicationHttpServlet extends TwdbHttpServlet {
         try (final OutputStream respOutputStream = resp.getOutputStream()) {
             RDFDataMgr.write(respOutputStream, nanopublication.get().toDataset(), respLang);
         }
+    }
+
+    private Optional<Uri> getNanopublicationUri(final HttpServletRequest req) {
+        @Nullable final String pathInfo = req.getPathInfo();
+        if (pathInfo == null || pathInfo.length() < 2) {
+            return Optional.empty();
+        }
+
+        return Optional.of(Uris.parse(pathInfo.substring(1)));
+    }
+
+    private Lang getRequestLang(final HttpServletRequest req) throws IOException {
+        @Nullable final String contentType = req.getContentType();
+        if (contentType == null || contentType.isEmpty()) {
+            throw new IllegalArgumentException("missing Content-Type");
+        }
+
+        @Nullable final Lang reqLang = RDFLanguages.contentTypeToLang(contentType);
+        if (reqLang == null) {
+            logger.error("PUT request has a non-RDF Content-Type: {}", contentType);
+            throw new IllegalArgumentException(contentType);
+        }
+
+        return reqLang;
+    }
+
+    private Optional<NanopublicationDialect> getRequestNanopublicationDialect(final HttpServletRequest req) {
+        final @Nullable String dialectString = req.getHeader("X-Nanopublication-Dialect");
+        if (dialectString == null) {
+            return Optional.empty();
+        }
+
+        return Optional.of(NanopublicationDialect.valueOf(dialectString.toUpperCase()));
+    }
+
+    @Override
+    protected void doPut(final HttpServletRequest req, final HttpServletResponse resp) throws IOException {
+        final Nanopublication nanopublication;
+        try {
+            nanopublication = parseRequestNanopublication(req, resp);
+        } catch (final IllegalArgumentException | MalformedNanopublicationException e) {
+            logger.error("error parsing nanopublication from PUT request: ", e);
+            return;
+        }
+
+        getDb().putNanopublication(nanopublication);
+
+        resp.setStatus(HttpServletResponse.SC_NO_CONTENT);
+    }
+
+    private Nanopublication parseRequestNanopublication(final HttpServletRequest req, final HttpServletResponse resp) throws MalformedNanopublicationException, IOException {
+        final Lang reqLang = getRequestLang(req);
+
+        final NanopublicationParser parser = new NanopublicationParser();
+        parser.setLang(reqLang);
+
+        final Optional<NanopublicationDialect> dialect = getRequestNanopublicationDialect(req);
+        if (dialect.isPresent()) {
+            parser.setDialect(dialect.get());
+        }
+
+        final Optional<Uri> nanopublicationUri = getNanopublicationUri(req);
+
+        final String reqBody;
+        try (final Reader reqReader = req.getReader()) {
+            reqBody = CharStreams.toString(req.getReader());
+        }
+
+        return parser.parse(new StringReader(reqBody), nanopublicationUri);
     }
 }
