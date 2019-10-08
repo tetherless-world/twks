f05d6dc8262bb885a0b2e6dccbb89787e47547c2
Minor Gordon <gordom6@rpi.edu>
2019-10-08 11:05:09 -0400

    expanding Jersey tests

--- java/server/src/test/java/edu/rpi/tw/twdb/server/resource/nanopublication/NanopublicationResourceTest.java
+++ java/server/src/test/java/edu/rpi/tw/twdb/server/resource/nanopublication/NanopublicationResourceTest.java
@@ -5,15 +5,20 @@ import edu.rpi.tw.nanopub.Uris;
 import edu.rpi.tw.twdb.api.Twdb;
 import edu.rpi.tw.twdb.server.AbstractResourceTest;
 import org.apache.jena.query.Dataset;
+import org.apache.jena.rdf.model.Model;
 import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
 import org.junit.Test;
 
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.Invocation;
+import javax.ws.rs.core.Response;
 import java.io.StringWriter;
 import java.net.URLEncoder;
 
+import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public final class NanopublicationResourceTest extends AbstractResourceTest {
     private static String toTrigString(final Nanopublication nanopublication) {
@@ -21,6 +26,20 @@ public final class NanopublicationResourceTest extends AbstractResourceTest {
         final StringWriter stringWriter = new StringWriter();
         RDFDataMgr.write(stringWriter, dataset, Lang.TRIG);
         return stringWriter.toString();
+    }
+
+    private static String toTrigString(final Model model) {
+        final StringWriter stringWriter = new StringWriter();
+        RDFDataMgr.write(stringWriter, model, Lang.TRIG);
+        return stringWriter.toString();
+    }
+
+    private static Entity<String> toTrigEntity(final Nanopublication nanopublication) {
+        return Entity.entity(toTrigString(nanopublication), Lang.TRIG.getContentType().getContentType());
+    }
+
+    private static Entity<String> toTrigEntity(final Model model) {
+        return Entity.entity(toTrigString(model), Lang.TRIG.getContentType().getContentType());
     }
 
     @Test
@@ -31,12 +50,64 @@ public final class NanopublicationResourceTest extends AbstractResourceTest {
         assertEquals(toTrigString(getTestData().specNanopublication), responseBody);
     }
 
+    @Test
+    public void testGetNanopublicationAbsent() throws Exception {
+        final Response response = target()
+                .path("/nanopublication/")
+                .path(URLEncoder.encode(Uris.toString(getTestData().specNanopublication.getUri()), "UTF-8"))
+                .request(Lang.TRIG.getContentType().getContentType())
+                .get();
+        assertEquals(Response.Status.NOT_FOUND, response.getStatus());
+    }
+
     @Override
     protected Object newResource(final Twdb db) {
         return new NanopublicationResource(db);
     }
 
     @Test
-    public void testPutNanopublicationWithUri() {
+    public void testPutNanopublicationWithUri() throws Exception {
+        final Response response =
+                target()
+                        .path("/nanopublication/")
+                        .path(URLEncoder.encode(Uris.toString(getTestData().specNanopublication.getUri()), "UTF-8"))
+                        .request()
+                        .put(toTrigEntity(getTestData().specNanopublication));
+        assertEquals(Response.Status.CREATED.getStatusCode(), response.getStatus());
+        assertTrue(getDb().getNanopublication(getTestData().specNanopublication.getUri()).isPresent());
+    }
+
+    @Test
+    public void testPutNanopublicationWithoutUri() throws Exception {
+        final Response response =
+                target()
+                        .path("/nanopublication/")
+                        .request()
+                        .put(toTrigEntity(getTestData().specNanopublication));
+        assertEquals(Response.Status.CREATED.getStatusCode(), response.getStatus());
+        assertTrue(getDb().getNanopublication(getTestData().specNanopublication.getUri()).isPresent());
+    }
+
+    @Test
+    public void testPutAssertionsWithoutUri() {
+        final Response response =
+                target()
+                        .path("/nanopublication/")
+                        .request()
+                        .put(toTrigEntity(getTestData().specNanopublication.getAssertion().getModel()));
+        assertEquals(Response.Status.BAD_REQUEST.getStatusCode(), response.getStatus());
+        assertFalse(getDb().getNanopublication(getTestData().specNanopublication.getUri()).isPresent());
+    }
+
+    @Test
+    public void testPutAssertionsWithUri() throws Exception {
+        final Response response =
+                target()
+                        .path("/nanopublication/")
+                        .path(URLEncoder.encode(Uris.toString(getTestData().specNanopublication.getUri()), "UTF-8"))
+                        .request()
+                        .put(toTrigEntity(getTestData().specNanopublication.getAssertion().getModel()));
+        assertEquals(Response.Status.CREATED.getStatusCode(), response.getStatus());
+        assertTrue(getDb().getNanopublication(getTestData().specNanopublication.getUri()).isPresent());
     }
 }
