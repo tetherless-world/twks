7c4d7cb8b9d44f7b358567e1772317101c249428
Minor Gordon <gordom6@rpi.edu>
2019-10-11 14:35:33 -0400

    back into client

--- java/client/src/main/java/edu/rpi/tw/twks/client/TwksClient.java
+++ java/client/src/main/java/edu/rpi/tw/twks/client/TwksClient.java
@@ -1,4 +1,47 @@
 package edu.rpi.tw.twks.client;
 
-public interface TwksClient {
+import com.google.api.client.http.javanet.NetHttpTransport;
+import edu.rpi.tw.twks.api.NanopublicationCrudApi;
+import edu.rpi.tw.twks.api.SparqlQueryApi;
+import edu.rpi.tw.twks.nanopub.Nanopublication;
+import edu.rpi.tw.twks.uri.Uri;
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+
+import java.util.Optional;
+
+/**
+ * Client for a TWKS server.
+ */
+public final class TwksClient implements NanopublicationCrudApi, SparqlQueryApi {
+    private final NetHttpTransport httpTransport;
+
+    public TwksClient() {
+        httpTransport = new NetHttpTransport();
+    }
+
+    @Override
+    public DeleteNanopublicationResult deleteNanopublication(final Uri uri) {
+        return null;
+    }
+
+    @Override
+    public Optional<Nanopublication> getNanopublication(final Uri uri) {
+        return Optional.empty();
+    }
+
+    @Override
+    public PutNanopublicationResult putNanopublication(final Nanopublication nanopublication) {
+        return null;
+    }
+
+    @Override
+    public QueryExecution queryAssertions(final Query query) {
+        return null;
+    }
+
+    @Override
+    public QueryExecution queryNanopublications(final Query query) {
+        return null;
+    }
 }
