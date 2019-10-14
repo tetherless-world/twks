9cd27e8d361a799e2ba79d29f6886a8084a317f3
Minor Gordon <gordom6@rpi.edu>
2019-10-14 14:35:58 -0400

    setting up PyPI machinery

---
+++ py/release.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rm -fr build dist *.egg-info
+python3 setup.py sdist bdist_wheel
+python3 -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
--- py/setup.py
+++ py/setup.py
@@ -1,17 +1,28 @@
+import os.path
+
 from setuptools import setup
 
+with open(os.path.join(os.path.dirname(__file__), "README.md"), "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name="twks-client",
-    version="1.0.0",
     author="Tetherless World Constellation",
-    license="Apache License 2.0",
-    packages=['twks', 'twks.client', 'twks.nanopub'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    description="Tetherless World Knowledge Store (TWKS) client",
     install_requires=[
         'rdflib>=4',
         'SPARQLWrapper>=1,<2',
         'requests>=2,<3',
     ],
-    classifiers=[
-        "License :: OSI Approved :: Apache Software License",
-    ],
+    license="Apache License 2.0",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    name="twks-client",
+    packages=['twks', 'twks.client', 'twks.nanopub'],
+    url="https://github.com/tetherless-world/twks",
+    version="1.0.0",
 )
