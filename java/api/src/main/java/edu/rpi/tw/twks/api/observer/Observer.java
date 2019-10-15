2ae9c7c2bb1785fa3d460c9915bd6b0b1cdee951
Minor Gordon <gordom6@rpi.edu>
2019-10-15 16:55:55 -0400

    documentation

--- java/api/src/main/java/edu/rpi/tw/twks/api/observer/Observer.java
+++ java/api/src/main/java/edu/rpi/tw/twks/api/observer/Observer.java
@@ -2,6 +2,10 @@ package edu.rpi.tw.twks.api.observer;
 
 /**
  * Parent interface of all observer callback interfaces.
+ * <p>
+ * There are multiple observer child interfaces instead of a single Observer interface with all of the on* so that
+ * - implementations can choose what they observe
+ * - adding new on* methods will not break implementations
  */
 public interface Observer {
 }
