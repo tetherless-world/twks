69334cc5515e62cb6beed3d7789b878be931ebaf
Minor Gordon <gordom6@rpi.edu>
2019-10-17 15:06:46 -0400

    OWLRL reasoner

--- py/examples/extfs/change/owlrl_reasoner.py
+++ py/examples/extfs/change/owlrl_reasoner.py
@@ -4,6 +4,8 @@ An example change observer. This will be invoked on any change to the store, suc
 
 import logging
 
+import rdflib
+
 from twks.ext import ExtensionArgumentParser
 
 
@@ -18,7 +20,19 @@ def main():
     client = args.client
 
     assertions_graph = client.get_assertions()
-    print(len(assertions_graph))
+    print(len(assertions_graph), "assertions at start")
+    expanded_assertions_graph = rdflib.Graph()
+    for assertion in assertions_graph:
+        expanded_assertions_graph.add(assertion)
+
+    reasoner = owlrl.DeductiveClosure(owlrl.OWLRL_Extension)
+    reasoner.expand(expanded_assertions_graph)
+    print(len(expanded_assertions_graph), "assertions in expanded graph")
+
+    new_assertions = expanded_assertions_graph - assertions_graph
+    print(len(new_assertions), "new assertions")
+    # for assertion in new_assertions:
+    #     print(assertion)
 
 
 if __name__ == "__main__":
