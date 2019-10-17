fb1b23d8b0d9052edcd2a38151dc5c284f546f43
Minor Gordon <gordom6@rpi.edu>
2019-10-17 14:53:59 -0400

    py: Nanopublication.parse_assertions and .from_assertions, duplicates functionality in the Java library

--- py/tests/conftest.py
+++ py/tests/conftest.py
@@ -6,6 +6,11 @@ from twks.nanopub.nanopublication import Nanopublication
 
 
 @pytest.fixture
+def assertions_ttl_file_path():
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), "assertions.ttl"))
+
+
+@pytest.fixture
 def spec_nanopublication_trig_file_path():
     return os.path.abspath(os.path.join(os.path.dirname(__file__), "spec_nanopublication.trig"))
 
--- py/tests/twks_test/nanopub/test_nanopublication.py
+++ py/tests/twks_test/nanopub/test_nanopublication.py
@@ -1,8 +1,19 @@
+import pathlib
+
+import rdflib
+
 from twks.nanopub.nanopublication import Nanopublication
 
 
 def test_parse(spec_nanopublication_trig_file_path):
     nanopublication = Nanopublication.parse(source=spec_nanopublication_trig_file_path)
+    assert isinstance(nanopublication, Nanopublication)
+
+
+def test_parse_assertions(assertions_ttl_file_path):
+    nanopublication = Nanopublication.parse_assertions(format="ttl", source=assertions_ttl_file_path,
+                                                       nanopublication_uri=rdflib.URIRef(pathlib.Path(
+                                                           assertions_ttl_file_path).as_uri()))
     assert isinstance(nanopublication, Nanopublication)
 
 
