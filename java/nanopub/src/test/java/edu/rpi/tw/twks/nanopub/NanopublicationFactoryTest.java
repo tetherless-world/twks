8e4f3fcc83d1b0120df63a20939f99b24c60ae58
Minor Gordon <gordom6@rpi.edu>
2019-10-18 17:09:57 -0400

    unit test

--- java/nanopub/src/test/java/edu/rpi/tw/twks/nanopub/NanopublicationFactoryTest.java
+++ java/nanopub/src/test/java/edu/rpi/tw/twks/nanopub/NanopublicationFactoryTest.java
@@ -1,8 +1,12 @@
 package edu.rpi.tw.twks.nanopub;
 
 import com.google.common.collect.ImmutableList;
+import org.apache.jena.query.Dataset;
+import org.apache.jena.query.DatasetFactory;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
 
@@ -17,12 +21,22 @@ public final class NanopublicationFactoryTest {
     }
 
     @Test
-    public void testCreateNanopublicationsFromDataset() throws MalformedNanopublicationException {
+    public void testCreateNanopublicationFromDataset() throws MalformedNanopublicationException {
         final ImmutableList<Nanopublication> nanopublications = sut.createNanopublicationsFromDataset(testData.specNanopublicationDataset);
         assertEquals(1, nanopublications.size());
         final Nanopublication nanopublication = nanopublications.get(0);
         assertEquals(1, nanopublication.getAssertion().getModel().listStatements().toList().size());
         assertEquals(3, nanopublication.getProvenance().getModel().listStatements().toList().size());
         assertEquals(2, nanopublication.getPublicationInfo().getModel().listStatements().toList().size());
+    }
+
+    @Test
+    public void testCreateNanopublicationsFromDataset() throws MalformedNanopublicationException, IOException {
+        final Dataset dataset = DatasetFactory.create();
+        new NanopublicationParser().parse(testData.assertionOnlyFilePath).get(0).toDataset(dataset);
+        sut.createNanopublicationsFromDataset(testData.specNanopublicationDataset).get(0).toDataset(dataset);
+        assertEquals(8, ImmutableList.copyOf(dataset.listNames()).size());
+        final ImmutableList<Nanopublication> nanopublications = NanopublicationFactory.getInstance().createNanopublicationsFromDataset(dataset);
+        assertEquals(2, nanopublications.size());
     }
 }
