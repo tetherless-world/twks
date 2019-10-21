ab28a65ab9eb06f3dbbf10406e0ccaaf6913e177
Minor Gordon <gordom6@rpi.edu>
2019-10-21 14:05:50 -0400

    redirect to file

--- .circleci/lint-md.sh
+++ .circleci/lint-md.sh
@@ -8,4 +8,4 @@ sudo gem install --silent mdl
 find . -name README.md | xargs mdl >>lint-results/mdl.txt 2>&1
 
 sudo npm install -g --silent markdown-link-check
-find . -name README.md -exec markdown-link-check {} \;
+find . -name README.md -exec markdown-link-check {} \; >>lint-results/markdown-link-check.txt
