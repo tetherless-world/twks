ebbad4c5e240780e0c4df81452c6cb5a13d431b8
Minor Gordon <gordom6@rpi.edu>
2019-10-21 14:00:07 -0400

    fix Circle conf

--- .circleci/lint-md.sh
+++ .circleci/lint-md.sh
@@ -8,4 +8,4 @@ sudo gem install --silent mdl
 find . -name README.md | xargs mdl >>lint-results/mdl.txt 2>&1
 
 sudo npm install -g --silent markdown-link-check
-find . -name README.md | xargs markdown-link-check >>lint-results/markdown-lint-check 2>&1
+find . -name README.md | xargs -0 -n 1 markdown-link-check >>lint-results/markdown-lint-check.txt 2>&1
