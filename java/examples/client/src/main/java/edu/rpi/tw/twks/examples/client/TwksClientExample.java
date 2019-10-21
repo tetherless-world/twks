e6cf8cf674190279726813dd2902f0c3ee04b3eb
Minor Gordon <gordom6@rpi.edu>
2019-10-21 14:47:35 -0400

    working client example

--- java/examples/client/src/main/java/edu/rpi/tw/twks/examples/client/TwksClientExample.java
+++ java/examples/client/src/main/java/edu/rpi/tw/twks/examples/client/TwksClientExample.java
@@ -5,10 +5,14 @@ import edu.rpi.tw.twks.client.TwksClientConfiguration;
 import edu.rpi.tw.twks.nanopub.MalformedNanopublicationException;
 import edu.rpi.tw.twks.nanopub.Nanopublication;
 import edu.rpi.tw.twks.nanopub.NanopublicationParser;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.rdf.model.Model;
 import org.apache.jena.riot.Lang;
 
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.Optional;
 
 public final class TwksClientExample {
     public final static String SPEC_NANOPUBLICATION_TRIG = "@prefix : <http://example.org/pub1#> .\n" +
@@ -49,8 +53,54 @@ public final class TwksClientExample {
 
         // Parse a nanopublication to use
         // The parser may return more than one, but we know there's only one.
+        System.out.println("Parsing the nanopublication");
         final Nanopublication nanopublication = new NanopublicationParser().setLang(Lang.TRIG).parse(new StringReader(SPEC_NANOPUBLICATION_TRIG)).get(0);
+        System.out.println("Parsed the nanopublication");
 
+        {
+            // Put the nanopublication to the server
+            System.out.println("Putting the nanopublication");
+            client.putNanopublication(nanopublication);
+            System.out.println("Put the nanopublication");
+        }
 
+        {
+            // Get the nanopublication back
+            System.out.println("Getting the nanopublication back");
+            final Nanopublication copy = client.getNanopublication(nanopublication.getUri()).get();
+            if (!copy.isIsomorphicWith(nanopublication)) {
+                throw new IllegalStateException("should be isomorphic");
+            }
+            System.out.println("Got the nanopublication back");
+        }
+
+        {
+            // Query assertions only
+            System.out.println("Assertions:");
+            final Model allAssertions = client.queryAssertions(QueryFactory.create("CONSTRUCT WHERE { ?s ?p ?o }")).execConstruct();
+            allAssertions.write(System.out, Lang.TTL.getName());
+            // Convenient equivalent:
+            System.out.println("Same assertions");
+            client.getAssertions().write(System.out, Lang.TTL.getName());
+        }
+
+        {
+            // Query over the entire nanopublication store
+            final ResultSet graphNames = client.queryNanopublications(QueryFactory.create("SELECT DISTINCT ?g WHERE { GRAPH ?g { ?s ?p ?o } }")).execSelect();
+            while (graphNames.hasNext()) {
+                System.out.println(graphNames.nextSolution().getResource("g"));
+            }
+        }
+
+        {
+            // Delete the nanopublication
+            System.out.println("Deleting the nanopublication");
+            client.deleteNanopublication(nanopublication.getUri());
+            System.out.println("Deleted the nanopublication");
+            final Optional<Nanopublication> copy = client.getNanopublication(nanopublication.getUri());
+            if (copy.isPresent()) {
+                throw new IllegalStateException("should not be present");
+            }
+        }
     }
 }
