64de828635cf98398bea16b4dc94720cdebe8c6c
Minor Gordon <gordom6@rpi.edu>
2019-10-22 15:13:18 -0400

    adapt paths

--- py/examples/extfs/observer/put_nanopublication/put_nanopublication_example.sh
+++ py/examples/extfs/observer/put_nanopublication/put_nanopublication_example.sh
@@ -2,12 +2,12 @@
 
 MY_DIR_PATH=$(dirname "$0")
 
-VENV_DIR_PATH="${MY_DIR_PATH}/../../../venv"
+VENV_DIR_PATH="${MY_DIR_PATH}/../../../../venv"
 
 if [ -d $VENV_DIR_PATH ]; then
   source $VENV_DIR_PATH/bin/activate
 fi
 
-export PYTHONPATH="${MY_DIR_PATH}/../../.."
+export PYTHONPATH="${MY_DIR_PATH}/../../../.."
 
 python3 $MY_DIR_PATH/put_nanopublication_example.py $*
