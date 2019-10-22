404b9cb2988b67b27f4715a8cd6d6604c382d129
Minor Gordon <gordom6@rpi.edu>
2019-10-22 13:17:58 -0400

    fix paths

--- .circleci/test-java.sh
+++ .circleci/test-java.sh
@@ -8,12 +8,12 @@ MAVEN_OPTS="-Xmx3500M" \
   -DargLine=\"-Dtwks.serverBaseUrl=http://twks-server:8080\" -Dtwks-client.skipTests=false \
   test
 mkdir surefire-reports
-mv java/cli/target/surefire-reports/* surefire-reports
-mv java/ext/target/surefire-reports/* surefire-reports
-mv java/factory/target/surefire-reports/* surefire-reports
-mv java/nanopub/target/surefire-reports/* surefire-reports
-mv java/server/target/surefire-reports/* surefire-reports
-mv java/tdb/target/surefire-reports/* surefire-reports
-mv java/uri/target/surefire-reports/* surefire-reports
+mv cli/target/surefire-reports/* surefire-reports
+mv ext/target/surefire-reports/* surefire-reports
+mv factory/target/surefire-reports/* surefire-reports
+mv nanopub/target/surefire-reports/* surefire-reports
+mv server/target/surefire-reports/* surefire-reports
+mv tdb/target/surefire-reports/* surefire-reports
+mv uri/target/surefire-reports/* surefire-reports
 tar cf surefire-reports
 bzip2 -9 surefire-reports.tar
