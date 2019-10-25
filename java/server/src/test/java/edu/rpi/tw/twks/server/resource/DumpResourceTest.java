9ea50df4e0e3cc275972bcb659c31f68b979f1b9
Minor Gordon <gordom6@rpi.edu>
2019-10-25 13:14:10 -0400

    fix regression

--- java/server/src/test/java/edu/rpi/tw/twks/server/resource/DumpResourceTest.java
+++ java/server/src/test/java/edu/rpi/tw/twks/server/resource/DumpResourceTest.java
@@ -5,19 +5,20 @@ import edu.rpi.tw.twks.server.AbstractResourceTest;
 import edu.rpi.tw.twks.test.ApisTest;
 import org.junit.Test;
 
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Response;
 
 import static org.junit.Assert.assertEquals;
 
 public final class DumpResourceTest extends AbstractResourceTest {
     @Test
-    public void testGetDump() throws Exception {
+    public void testPostDump() throws Exception {
         getTwks().putNanopublication(getTestData().specNanopublication);
         final Response response =
                 target()
                         .path("/dump")
                         .request()
-                        .get();
+                        .post(Entity.json(""));
         assertEquals(200, response.getStatus());
         ApisTest.checkDump(getTempDirPath());
     }
