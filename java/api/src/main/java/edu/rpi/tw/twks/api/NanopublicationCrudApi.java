9978b5c423f5fa782347db541fc626d7358f35fb
Minor Gordon <gordom6@rpi.edu>
2019-10-28 12:23:22 -0400

    API definition

--- java/api/src/main/java/edu/rpi/tw/twks/api/NanopublicationCrudApi.java
+++ java/api/src/main/java/edu/rpi/tw/twks/api/NanopublicationCrudApi.java
@@ -38,8 +38,17 @@ public interface NanopublicationCrudApi {
      * Put a new nanopublication, overwriting an existing nanopublication with the same URI if necessary.
      *
      * @param nanopublication nanopublication to put.
+     * @return result of the operation
      */
     PutNanopublicationResult putNanopublication(Nanopublication nanopublication);
+
+    /**
+     * Put a list of nanopublications. See putNanopublication for semantics.
+     *
+     * @param nanopublications nanopublications to put
+     * @return list of results, guaranteed to be the same size as the input list
+     */
+    ImmutableList<PutNanopublicationResult> putNanopublications(ImmutableList<Nanopublication> nanopublications);
 
     enum DeleteNanopublicationResult {
         DELETED, NOT_FOUND
