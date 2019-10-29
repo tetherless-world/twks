5165bc7df11446c7f3bba4b08510030f328455fb
Minor Gordon <gordom6@rpi.edu>
2019-10-29 09:32:56 -0400

    add isAbout property

--- java/vocabulary/src/main/java/edu/rpi/tw/twks/vocabulary/SIO.java
+++ java/vocabulary/src/main/java/edu/rpi/tw/twks/vocabulary/SIO.java
@@ -1,8 +1,13 @@
 package edu.rpi.tw.twks.vocabulary;
 
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.ResourceFactory;
+
 public final class SIO {
-    public final static String PREFIX = "sio";
     public final static String NS = "http://semanticscience.org/resource/";
+    public final static String PREFIX = "sio";
+    // Properties
+    public final static Property isAbout = ResourceFactory.createProperty(NS + "isAbout");
 
     private SIO() {
     }
