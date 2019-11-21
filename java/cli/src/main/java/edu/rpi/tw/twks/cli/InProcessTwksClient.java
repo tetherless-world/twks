e31aaa35a040aedda9b27367b6648d7521fcb229
Minor Gordon <gordom6@rpi.edu>
2019-11-21 11:48:46 -0500

    implement InProcessTwksClient

--- java/cli/src/main/java/edu/rpi/tw/twks/cli/InProcessTwksClient.java
+++ java/cli/src/main/java/edu/rpi/tw/twks/cli/InProcessTwksClient.java
@@ -3,15 +3,23 @@ package edu.rpi.tw.twks.cli;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import edu.rpi.tw.twks.api.Twks;
+import edu.rpi.tw.twks.api.TwksTransaction;
 import edu.rpi.tw.twks.client.TwksClient;
 import edu.rpi.tw.twks.nanopub.Nanopublication;
 import edu.rpi.tw.twks.uri.Uri;
-import org.apache.jena.query.Query;
-import org.apache.jena.query.QueryExecution;
+import org.apache.jena.atlas.json.JsonArray;
+import org.apache.jena.atlas.json.JsonObject;
+import org.apache.jena.graph.Triple;
+import org.apache.jena.query.*;
 import org.apache.jena.rdf.model.Model;
+import org.apache.jena.sparql.core.Quad;
+import org.apache.jena.sparql.engine.binding.Binding;
+import org.apache.jena.sparql.util.Context;
 
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
@@ -64,11 +72,161 @@ public final class InProcessTwksClient implements TwksClient {
 
     @Override
     public QueryExecution queryAssertions(final Query query) {
-        return null;
+        final TwksTransaction transaction = delegate.beginTransaction(ReadWrite.READ);
+        final QueryExecution queryExecution = transaction.queryAssertions(query);
+        return new TransactionalQueryExecution(queryExecution, transaction);
     }
 
     @Override
     public QueryExecution queryNanopublications(final Query query) {
-        return null;
+        final TwksTransaction transaction = delegate.beginTransaction(ReadWrite.READ);
+        final QueryExecution queryExecution = transaction.queryNanopublications(query);
+        return new TransactionalQueryExecution(queryExecution, transaction);
+    }
+
+    private final static class TransactionalQueryExecution implements QueryExecution {
+        private final QueryExecution delegate;
+        private final TwksTransaction transaction;
+
+        private TransactionalQueryExecution(final QueryExecution delegate, final TwksTransaction transaction) {
+            this.delegate = delegate;
+            this.transaction = transaction;
+        }
+
+        @Override
+        public void abort() {
+            delegate.abort();
+        }
+
+        @Override
+        public void close() {
+            delegate.close();
+            transaction.close();
+        }
+
+        @Override
+        public boolean execAsk() {
+            return delegate.execAsk();
+        }
+
+        @Override
+        public Model execConstruct() {
+            return delegate.execConstruct();
+        }
+
+        @Override
+        public Model execConstruct(final Model model) {
+            return delegate.execConstruct(model);
+        }
+
+        @Override
+        public Dataset execConstructDataset() {
+            return delegate.execConstructDataset();
+        }
+
+        @Override
+        public Dataset execConstructDataset(final Dataset dataset) {
+            return delegate.execConstructDataset(dataset);
+        }
+
+        @Override
+        public Iterator<Quad> execConstructQuads() {
+            return delegate.execConstructQuads();
+        }
+
+        @Override
+        public Iterator<Triple> execConstructTriples() {
+            return delegate.execConstructTriples();
+        }
+
+        @Override
+        public Model execDescribe() {
+            return delegate.execDescribe();
+        }
+
+        @Override
+        public Model execDescribe(final Model model) {
+            return delegate.execDescribe(model);
+        }
+
+        @Override
+        public Iterator<Triple> execDescribeTriples() {
+            return delegate.execDescribeTriples();
+        }
+
+        @Override
+        public JsonArray execJson() {
+            return delegate.execJson();
+        }
+
+        @Override
+        public Iterator<JsonObject> execJsonItems() {
+            return delegate.execJsonItems();
+        }
+
+        @Override
+        public ResultSet execSelect() {
+            return delegate.execSelect();
+        }
+
+        @Override
+        public Context getContext() {
+            return delegate.getContext();
+        }
+
+        @Override
+        public Dataset getDataset() {
+            return delegate.getDataset();
+        }
+
+        @Override
+        public Query getQuery() {
+            return delegate.getQuery();
+        }
+
+        @Override
+        public long getTimeout1() {
+            return delegate.getTimeout1();
+        }
+
+        @Override
+        public long getTimeout2() {
+            return delegate.getTimeout2();
+        }
+
+        @Override
+        public boolean isClosed() {
+            return delegate.isClosed();
+        }
+
+        @Override
+        public void setInitialBinding(final QuerySolution querySolution) {
+            delegate.setInitialBinding(querySolution);
+        }
+
+        @Override
+        public void setInitialBinding(final Binding binding) {
+            delegate.setInitialBinding(binding);
+        }
+
+        @Override
+        public void setTimeout(final long l, final TimeUnit timeUnit) {
+            delegate.setTimeout(l, timeUnit);
+        }
+
+        @Override
+        public void setTimeout(final long l) {
+            delegate.setTimeout(l);
+        }
+
+        @Override
+        public void setTimeout(final long l, final TimeUnit timeUnit, final long l1, final TimeUnit timeUnit1) {
+            delegate.setTimeout(l, timeUnit, l1, timeUnit1);
+        }
+
+        @Override
+        public void setTimeout(final long l, final long l1) {
+            delegate.setTimeout(l, l1);
+        }
     }
 }
