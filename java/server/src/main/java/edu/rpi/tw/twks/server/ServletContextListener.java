c65777516d83b45a16352bde754f88d101113c4c
Minor Gordon <gordom6@rpi.edu>
2020-03-25 17:39:20 -0400

    do initial load

--- java/server/src/main/java/edu/rpi/tw/twks/server/ServletContextListener.java
+++ java/server/src/main/java/edu/rpi/tw/twks/server/ServletContextListener.java
@@ -1,10 +1,13 @@
 package edu.rpi.tw.twks.server;
 
+import com.google.common.collect.ImmutableList;
 import edu.rpi.tw.twks.api.Twks;
 import edu.rpi.tw.twks.api.TwksLibraryVersion;
 import edu.rpi.tw.twks.ext.ClasspathExtensions;
 import edu.rpi.tw.twks.ext.FileSystemExtensions;
 import edu.rpi.tw.twks.factory.TwksFactory;
+import edu.rpi.tw.twks.nanopub.Nanopublication;
+import edu.rpi.tw.twks.nanopub.NanopublicationParser;
 import edu.rpi.tw.twks.servlet.JerseyServlet;
 import org.apache.commons.configuration2.web.ServletContextConfiguration;
 import org.slf4j.Logger;
@@ -13,6 +16,7 @@ import org.slf4j.LoggerFactory;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletRegistration;
+import java.nio.file.Path;
 import java.util.Optional;
 
 public final class ServletContextListener implements javax.servlet.ServletContextListener {
@@ -47,5 +51,35 @@ public final class ServletContextListener implements javax.servlet.ServletContex
         servletRegistration.addMapping("/*");
 
         logger.info("twks-server " + TwksLibraryVersion.getInstance());
+
+        loadInitialNanopublications(configuration, twks);
+    }
+
+    private void loadInitialNanopublications(final TwksServerConfiguration configuration, final Twks twks) {
+        final ImmutableList<Nanopublication> nanopublications;
+        {
+//        NanopublicationParser nanopublicationParser = NanopublicationParser.builder().setDialect(NanopublicationDialect.SPECIFICATION).setLang(Lang.TRIG).setIgnoreMalformedNanopublications(true).build();
+            final NanopublicationParser nanopublicationParser = NanopublicationParser.DEFAULT;
+
+            final ImmutableList.Builder<Nanopublication> nanopublicationsBuilder = ImmutableList.builder();
+
+            if (configuration.getInitialNanopublicationsDirectoryPath().isPresent()) {
+                nanopublicationsBuilder.addAll(nanopublicationParser.parseDirectory(configuration.getInitialNanopublicationsDirectoryPath().get().toFile()).values());
+            }
+
+            if (configuration.getInitialNanopublicationFilePaths().isPresent()) {
+                for (final Path nanopublicationFilePath : configuration.getInitialNanopublicationFilePaths().get()) {
+                    nanopublicationsBuilder.addAll(nanopublicationParser.parseFile(nanopublicationFilePath.toFile()));
+                }
+            }
+
+            nanopublications = nanopublicationsBuilder.build();
+            if (nanopublications.isEmpty()) {
+                return;
+            }
+        }
+
+        twks.postNanopublications(nanopublications);
+        logger.info("posted {} initial nanopublication(s)", nanopublications.size());
     }
 }
