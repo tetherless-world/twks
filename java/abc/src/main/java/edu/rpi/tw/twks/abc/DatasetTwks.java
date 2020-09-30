46c418df3eab0476baf1f9bc560a44febcf5f4eb
Minor Gordon <gordom6@rpi.edu>
2020-09-30 16:06:16 -0400

    make Twks AutoCloseable

--- java/abc/src/main/java/edu/rpi/tw/twks/abc/DatasetTwks.java
+++ java/abc/src/main/java/edu/rpi/tw/twks/abc/DatasetTwks.java
@@ -3,6 +3,8 @@ package edu.rpi.tw.twks.abc;
 import edu.rpi.tw.twks.configuration.TwksConfiguration;
 import org.apache.jena.query.Dataset;
 
+import java.io.IOException;
+
 import static com.google.common.base.Preconditions.checkNotNull;
 
 public abstract class DatasetTwks<TwksConfigurationT extends TwksConfiguration, TwksMetricsT extends QuadStoreTwksMetrics> extends QuadStoreTwks<TwksConfigurationT, TwksMetricsT> {
@@ -11,6 +13,11 @@ public abstract class DatasetTwks<TwksConfigurationT extends TwksConfiguration, 
     protected DatasetTwks(final TwksConfigurationT configuration, final Dataset dataset, final TwksMetricsT metrics) {
         super(configuration, metrics);
         this.dataset = checkNotNull(dataset);
+    }
+
+    @Override
+    public void close() throws IOException {
+        dataset.close();
     }
 
     final Dataset getDataset() {
